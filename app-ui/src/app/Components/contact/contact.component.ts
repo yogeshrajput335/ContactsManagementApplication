3c8e78edeade506133ee3cef150a111f1ab4689a
Yogesh Rajput <yogeshrajput335@gmail.com>
2024-10-21 20:38:34 +0530

    Added bootstrap, added UI for contact list, contact model

---
+++ app-ui/src/app/Components/contact/contact.component.ts
@@ -0,0 +1,87 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Contact } from 'src/app/Models/Contact';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  contacts: Contact[] = [];
+  @Input() title="";
+  displayStyle = "none"; 
+  displayDeleteStyle = "none"; 
+  contactForm: FormGroup;
+  editContactId: number | null = null;
+  deleteContactId: number | null = null;
+
+  constructor(private fb: FormBuilder) { 
+    this.contactForm = this.fb.group({ 
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+  newContact(){
+    console.log("Open dialog for new contact");
+  }
+  editContact(contact: Contact) {
+    this.editContactId = contact.id;
+    this.contactForm.setValue({
+      firstname: contact.firstname,
+      lastname: contact.lastname,
+      email: contact.email,
+    });
+    this.displayStyle = "block"; 
+  }
+  deleteContact(id:number){
+    console.log("Open confirmation to delete contact");
+  }
+  
+  openPopup() { 
+    this.displayStyle = "block"; 
+  } 
+  closePopup() { 
+    this.displayStyle = "none"; 
+  } 
+  openDeletePopup(contactId: number) { 
+    this.deleteContactId = contactId;
+    this.displayDeleteStyle = "block"; 
+  } 
+  closeDeletePopup(){ 
+    this.displayDeleteStyle = "none"; 
+  } 
+  updateContact(){
+    this.contacts.push(
+      {
+        id:1,
+        firstname:this.contactForm.value.firstname,
+        lastname:this.contactForm.value.lastname,
+        email:this.contactForm.value.email
+      })
+      this.displayStyle = "none"; 
+  }
+  addContact(){
+    this.contacts.push(
+      {
+        id:1,
+        firstname:this.contactForm.value.firstname,
+        lastname:this.contactForm.value.lastname,
+        email:this.contactForm.value.email
+      })
+      this.displayStyle = "none"; 
+  }
+  DeleteContact(){
+    if(this.deleteContactId){
+      console.log("Contact selected");
+    }else {
+      console.log("Contact not selected");
+    }
+    this.displayDeleteStyle = "none"; 
+  }
+}
--- app-ui/src/app/app.module.ts
+++ app-ui/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
